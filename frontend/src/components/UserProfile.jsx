b3898e128dca7c60a1f8d16174e4f8e6a2993ce4
tddag <danhtam0509@gmail.com>
2022-02-16 10:23:36 -0500

    update UserProfile component

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { useParams, useNavigate } from 'react-router-dom';
+import { GoogleLogout } from 'react-google-login';
+
+import { userCreatedPinsQuery, userQuery, userSavedPinsQuery } from '../utils/data';
+import { client } from '../client';
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
+
+
+const randomImage = 'https://source.unsplash.com/1600*900/?nature,photography,technology'
 
 const UserProfile = () => {
-  return <div>
-      UserProfile
-  </div>;
+  const [user, setUser] = useState(null);
+  const [pins, setPins] = useState(null);
+  const [text, setText] = useState('Created');
+  const [activeBtn, setActiveBtn] = useState('created');
+  const navigate = useNavigate();
+  const { userId } = useParams();
+
+  useEffect(() => {
+    const query = userQuery(userId);
+
+    client.fetch(query)
+      .then((data) => {
+        setUser(data[0]);  
+      })
+  }, [userId]);
+  
+
+  if(!user) {
+    return <Spinner message="Loading profile..."/>
+  }
+
+
+  return (
+    <div className="relative pb-2 h-full justify-center items-center">
+        <div className="flex flex-col pb-5">
+          <div className="relative flex flex-col mb-7">
+            <div className="flex flex-col justify-center items-center">
+              <img
+                src={randomImage}
+                className="w-full h-370"
+              />
+            </div>
+
+          </div>
+        </div>
+    </div>);
 };
 
 export default UserProfile;
